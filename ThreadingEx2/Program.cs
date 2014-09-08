8b0f8e13aed909309f30e5c9dc7cff0e0680b25f
Max Stripp <maxbstripp@gmail.com>
2014-09-08 09:41:51 +0200

    Bjarne made me do it

--- ThreadingEx2/Program.cs
+++ ThreadingEx2/Program.cs
@@ -1,17 +1,34 @@
 ﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using ThreadingEx2;
 
 namespace FindSmallest
 {
-    class Program
+    internal class Program
     {
 
-        private static readonly int[][] Data = new int[][]{
-            new[]{1,5,4,2}, 
-            new[]{3,2,4,11,4},
-            new[]{33,2,3,-1, 10},
-            new[]{3,2,8,9,-1},
-            new[]{1, 22,1,9,-3, 5}
+    Generator _generator = new Generator();
+    {
+      
+    }
+    
+  
+
+        private static readonly int[][] Data = new int[][]
+        {
+            new[] {1, 5, 4, 2},
+            new[] {3, 2, 4, 11, 4},
+            new[] {33, 2, 3, -1, 10},
+            new[] {3, 2, 8, 9, -1},
+            new[] {1, 22, 1, 9, -3, 5}
+            
         };
+
 
         private static int FindSmallest(int[] numbers)
         {
@@ -31,13 +48,73 @@ namespace FindSmallest
             return smallestSoFar;
         }
 
-        static void Main()
+        private static int FindLargest(int[] numbers)
         {
-            foreach (int[] data in Data)
+            if (numbers.Length < 1)
             {
-                int smallest = FindSmallest(data);
-                Console.WriteLine("\t" + String.Join(", ", data) + "\n-> " + smallest);
+                throw new ArgumentException("There must be at least one element in the array");
             }
+
+            int largestSoFar = numbers[0];
+            foreach (int number in numbers)
+            {
+                if (number > largestSoFar)
+                {
+                    largestSoFar = number;
+                }
+                
+            }
+            return largestSoFar;
+        }
+
+        private static void Main()
+        {
+         
+            List<Task<int>> Liste = new List<Task<int>>();
+
+
+            foreach (int[] littled in Data)
+            {
+                Task<int> minTredjeOpg = new Task<int>(() =>
+                {
+                    int smallest = FindSmallest(littled);
+                    Console.WriteLine(smallest);
+                    return smallest;
+
+                }
+                    );
+                minTredjeOpg.Start();
+                Liste.Add(minTredjeOpg);
+                //Thread t1 = new Thread(() =>
+                //{
+
+                //    int smallest = FindSmallest(littled);
+
+                //    Console.WriteLine(smallest);
+                //    // Console.WriteLine("\t" + String.Join(", ", littled) + "\n-> " + smallest);
+
+
+
+                //t1.Start();
+                // Console.ReadLine();
+
+            }
+            foreach (int[] biggerd in Data)
+            {
+                Task<int> minFjerdeOpg = new Task<int>(() =>
+                {
+                    int largest = FindLargest(biggerd);
+                    Console.WriteLine(largest);
+                    return largest;
+                }
+                );
+                minFjerdeOpg.Start();
+                
+                Liste.Add(minFjerdeOpg);
+            }
+
+            Task.WaitAll(Liste.ToArray());
+           
         }
     }
 }
